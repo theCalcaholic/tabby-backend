34225cc9b39285f9894843c42272a5961f8fa3e8
theCalcaholic <3knoeppl@informatik.uni-hamburg.de>
2017-09-14 12:40:53 +0200

    Create simple REST/JSON express server.

---
+++ server.js
@@ -0,0 +1,97 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var crypto = require('crypto');
+var cors = require('cors');
+
+let MODE = require('./runmode').new();
+MODE.development(true);
+
+var app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const profiles = [
+  {
+    id: "9as9u2ob3bonal",
+    tabs: [
+      {
+        title: "Tab 1",
+        content: "<p>Content of Tab 1</p><p>More content...</p>"
+      }
+    ]
+  }
+]
+
+app.get('/profiles', function(req, res, next) {
+  if(MODE.production()) {
+    res.status(503).send();
+  }
+  else {
+    res.json({"data": profiles});
+  }
+});
+
+app.get('/profiles/new', function(req, res) {
+  let length = 16
+  let newId;
+  do {
+    newId = crypto.randomBytes(Math.ceil(length * 3 / 4))
+      .toString('base64')
+      .slice(0, length)
+      .replace(/\+/g, '0')
+      .replace(/\//g, '0');
+  } while( getProfile(newId) )
+  let profile = {
+    id: newId,
+    tabs: []
+  };
+  profiles.push(profile)
+  res.json({data: profile});
+});
+
+app.all('/profiles/:id', function(req, res, next) {
+  let id = req.params['id'];
+  if( id == 'new' || getProfile(id) ) {
+    next();
+  }
+  else {
+    console.log(id + " not found in profiles.");
+    res.sendStatus(404);
+    res.send();
+  }
+});
+
+app.get('/profiles/:id', function(req, res) {
+  let id = req.params['id'];
+  res.json({"data": getProfile(id)});
+});
+
+app.put('/profiles/:id', function(req, res) {
+  let id = req.params['id'];
+  deleteProfile(id);
+  profiles.push(req.body);
+});
+
+function getProfile(id) {
+  let profile;
+  profiles.forEach((currentProfile) => {
+    if(currentProfile.id == id) {
+      profile = currentProfile;
+    }
+  });
+  return profile;
+}
+
+function deleteProfile(id) {
+  for(i = 0; i < profiles.length; i++)
+  {
+    if(profiles[i].id == id) {
+      profiles.splice(i, 1);
+    }
+  }
+}
+
+app.listen(3000, () => {
+  console.log('Tabby backend started.');
+  console.log('Listening on port 3000');
+})
