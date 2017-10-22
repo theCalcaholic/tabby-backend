8d088ea02cce7504520a2c9d0d99276a5af8ef60
Tobias Knöppler <3knoeppl@informatik.uni-hamburg.de>
2017-10-22 21:15:32 +0200

    Make update script more verbose.

--- upgrade_and_build.sh
+++ upgrade_and_build.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 cd "${0%/*}"
+
+echo stopping tabby-backend service... && \
 sudo systemctl stop tabby-backend.service && \
+echo done. && \
+echo Updating source... && \
 sudo su www-data -s /bin/sh -c "git pull" && \
-sudo su www-data -s /bin/sh -c "npm install"
+echo done. && \
+echo Installing node modules... && \
+sudo su www-data -s /bin/sh -c "npm install" && \
+echo done.
+echo building source... && \
 sudo su www-data -s /bin/sh -c "./node_modules/.bin/tsc -p ./tsconfig.json" && \
+echo done. && \
+echo starting tabby-backend service... && \
 sudo systemctl start tabby-backend.service
 
